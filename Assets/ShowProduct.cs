1cc119d49fc3b8f2a1891fac9125de549b712d3f
Colin <zephyr.raine@gmail.comp>
2020-09-10 14:17:25 +0200

    toggle bouton

---
+++ Assets/ShowProduct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowProduct : MonoBehaviour
+{
+    public GameObject product;
+    public void ToggleProductVisibility()
+    {
+        product.SetActive(!product.activeSelf);
+    }
+}
