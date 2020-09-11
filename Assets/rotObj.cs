291ad6e580376391a0dcecf872b8e03c8ef5ee99
Colin <zephyr.raine@gmail.comp>
2020-09-11 15:54:37 +0200

    ok

--- Assets/rotObj.cs
+++ Assets/rotObj.cs
@@ -9,7 +9,7 @@ public class rotObj : MonoBehaviour
     float velocity;
     private void FixedUpdate()
     {
-        transformToRotate.RotateAround(Vector3.up, rotSpeed*velocity*Time.deltaTime);
+        transformToRotate.RotateAround(transform.up, rotSpeed*velocity*Time.deltaTime);
         velocity *= 0.98f;
     }
     void OnMouseDrag()
