7ddaeab141459200c898e075995a4430017b680f
ZephyrRaine <zephyr.raine@gmail.com>
2020-09-14 00:29:18 +0200

    food swapper

---
+++ Assets/FoodDisplayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class FoodDisplayer : MonoBehaviour
+{
+    public TMP_Text foodTitle;
+    int current = 0;
+    private void Awake()
+    {
+        foodTitle = GameObject.FindGameObjectWithTag("foodTitle").GetComponent<TMP_Text>();
+        ChangeCurrentFood(0);
+    }
+
+    public void ChangeCurrentFood(int offset)
+    {
+        current += offset;
+        if (current < 0)
+            current = 2;
+        if (current > 2)
+            current = 0;
+
+        foreach (Transform t in transform)
+        {
+            t.gameObject.SetActive(t.GetSiblingIndex() == current);
+        }
+
+        foodTitle.text = transform.GetChild(current).name;
+    }
+
+}
---
+++ Assets/RequestChangeFood.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestChangeFood : MonoBehaviour
+{
+    FoodDisplayer _fd;
+    FoodDisplayer fd
+    {
+        get
+        {
+            if(_fd == null)
+                _fd = FindObjectOfType<FoodDisplayer>();
+            return _fd;
+        }
+    }
+    public void Change(int offset)
+    {
+        fd?.ChangeCurrentFood(offset);
+    }
+}
