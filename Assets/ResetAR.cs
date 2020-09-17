ff2274ed638b5fdf6faf0f7a1805611cc8d5eb3c
Colin <zephyr.raine@gmail.comp>
2020-09-17 11:33:36 +0200

    arsessionresetmaybe?

---
+++ Assets/ResetAR.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ResetAR : MonoBehaviour
+{
+    void Start()
+    {
+        var arSession = GetComponent<ARSession>();
+        arSession.Reset();
+    }
+
+
+
+}
